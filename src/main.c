8ce3aed7b3b5491bdaf5cf56b5560c88be34e7db
Erfan Mirshams <erfanmirshams@protonmail.com>
2024-08-17 23:09:23 +0330

    chore: clean main.c

--- src/main.c
+++ src/main.c
@@ -1,35 +1,19 @@
 #include "raylib.h"
+#include "general.h"
 
 int main(void) {
-  // Initialization
-  //--------------------------------------------------------------------------------------
-  const int screenWidth = 800;
-  const int screenHeight = 450;
+  InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "turtix");
 
-  InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
-
-  SetTargetFPS(60); // Set our game to run at 60 frames-per-second
-  //--------------------------------------------------------------------------------------
-
-  // Main game loop
   while (!WindowShouldClose()) {
-    // Start Drawing
-    //----------------------------------------------------------------------------------
     BeginDrawing();
 
-    ClearBackground(RAYWHITE);
+    ClearBackground(WHITE);
 
-    DrawText("Congrats! You created your first window!", 190, 200, 20,
-             LIGHTGRAY);
+    DrawText("Hello", 190, 200, 20,
+             BLACK);
 
     EndDrawing();
-    //----------------------------------------------------------------------------------
   }
-
-  // De-Initialization
-  //--------------------------------------------------------------------------------------
-  CloseWindow(); // Close window and OpenGL context
-  //--------------------------------------------------------------------------------------
-
+  CloseWindow();
   return 0;
 }
